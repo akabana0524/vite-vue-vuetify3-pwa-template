a2a08dfb6e5293e6b6580c8c19fa85e6b9e174e8
HR/大橋政也 <chopsticks@hoshinoresorts.com>
2024-01-07 23:40:56 +0900

    deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vite-vue-vuetify3-pwa-template/",
   plugins: [
     vue(),
     VitePWA({
